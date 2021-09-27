cc569ea930bd67f0b01626157711612a8ea7706d
309James <869026861@qq.com>
2021-09-27 15:21:27 +0800

    SVM update

--- SVM_1.m
+++ SVM_1.m
@@ -8,15 +8,15 @@ Y=label_train;
 train_num=330;
 test_num=21;
 
-model = fitcsvm(X,Y,'BoxConstraint',100,'KernelFunction','gaussian','KernelScale',12);
+model = fitcsvm(X(1:260,:),Y(1:260),'BoxConstraint',4,'KernelFunction','gaussian','KernelScale',0.676*2^0.5);
 % model = fitcsvm(X,Y,'KernelFunction','gaussian','KernelScale',2^0.5*2);
-[Y1,~]=predict(model,data_train);
+[Y1,~]=predict(model,data_train(261:330,:));
 count=0;
-for i=1:train_num
-    if Y1(i)==label_train(i)
+for i=1:70
+    if Y1(i)==label_train(260+i)
         count=count+1;
     end
 end
-accuracy=count/train_num;
+accuracy=count/70;
 
 [Y2,~]=predict(model,data_test);
